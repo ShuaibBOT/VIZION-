f08d6ed5408d3fd187d63827decf42dc0e123760
Shuaib Feroze <shuaib271102@gmail.com>
2022-05-01 23:56:23 +0530

    Added New Deep learning model

---
+++ Deep learning model NEW/HGRTestModel.py
@@ -0,0 +1,53 @@
+import cv2 as cv
+import pickle
+import numpy as np
+import mediapipe as mp
+import tensorflow as tf
+from tensorflow.python.ops.numpy_ops import np_config
+np_config.enable_numpy_behavior()
+import PIL
+from PIL import Image
+import io
+
+
+
+# load model
+from pathlib import Path
+HERE = Path(__file__).parent
+# with open(HERE/'pickle_model.pkl', 'rb') as f:
+#     model = pickle.load(f)
+# model = pickle.load(open('pickle_model.pkl', 'rb'))
+model = tf.keras.models.load_model('my_model')
+
+model.summary()
+classNames = ["Blank","Rotation","Scale","Translation","X Rotation","X Scale","X Translation","Y Rotation","Y Scale","Y Translation","Z Rotation","Z Scale","Z Translation"]
+cap = cv.VideoCapture(0)
+
+# def change_res(width, height):
+#     cap.set(3, width)
+#     cap.set(4, height)
+#
+# change_res()
+
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+
+while True:
+    success, img = cap.read()
+
+    # print(img)
+    if not success:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    img = cv.cvtColor(img,cv.COLOR_BGR2RGB)
+    img = cv.resize(img, (224, 224))
+    cv.imshow("Threshold_image", img)
+    cv.waitKey(1)
+    img = img.reshape(1,224,224,3)
+    prediction = model.predict(img)
+    print(prediction)
+    for i in range(13):
+        if prediction[0,i] == 1.0:
+            print(classNames[i])
