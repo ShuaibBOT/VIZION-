97fa0155673aa65cfda04ccbb674b6981b0ae0a0
Thusal Athauda <87170959+Thusalll@users.noreply.github.com>
2022-05-04 02:24:06 +0530

    new final

--- Plug-in/Positioning.cs
+++ Plug-in/Positioning.cs
@@ -6,21 +6,12 @@ public class Positioning : MonoBehaviour
 {
     public UDPReceive udpReceive;
     private GameObject handPoints;
-    int NumOfHand;
-    public string handType; 
+    string handType;
     string handType2;
-    public int positionXL;
-    public int positionYL;
     public string LHFingers;
-    string RHFingers;
-    string LHFingers2;
-    int positionXR;
-    int positionYR;
+    public string RHFingers;
+    public string RHFingers2;
     string handClass;
-    //bool num1_type = false;
-    int LHX;
-    int LHY;
-    //bool num2_type = false;
 
     // Start is called before the first frame update
     void Start()
@@ -32,45 +23,32 @@ public class Positioning : MonoBehaviour
     void Update()
     {
         string centerPoints = udpReceive.centerPoints;
-        centerPoints = centerPoints.Remove(0,1);
-        centerPoints = centerPoints.Remove(centerPoints.Length-1,1);
+        centerPoints = centerPoints.Remove(0, 1);
+        centerPoints = centerPoints.Remove(centerPoints.Length - 1, 1);
         print(centerPoints);
 
         string[] points = centerPoints.Split(',');
-        //positionX = int.Parse(points[0]); 
-        //positionY = int.Parse(points[1]);
-        //print(positionX); //xvalue of LH
-        //print(positionY); //y value of LH
-        
-        if (points.Length == 8)
+
+        if (points.Length == 6)
         {
-            handType = points[7].ToString();
-            print(handType);
+            RHFingers = points[0] + points[1] + points[2] + points[3] + points[4];
+            print(RHFingers);
 
-            LHFingers = points[2] + points[3] + points[4] + points[5] + points[6];
-            print(LHFingers);
-
-            positionXL = int.Parse(points[0]);
-            positionYL = int.Parse(points[1]);
-
-            LHX = positionXL; print(LHX);
-            LHY = positionYL; print(LHY);
-
-            if (LHFingers == " 0 0 0 0 0")
+            if (RHFingers == "1 1 1 1 1")
             {
-                Vector3 temp = new Vector3(0.01f, 0, 0);
+                Vector3 temp = new Vector3(0.01f, 0, 0); //positioning x axis
                 handPoints.transform.position += temp;
                 print("Positioning");
             }
-            else if(LHFingers == " 1 0 0 0 0")
+            else if (RHFingers == "0 0 0 0 0")
             {
                 Vector3 temp = new Vector3(0.1f, 0, 0); //rotate x axis
                 handPoints.transform.Rotate(temp);
                 print("Rotation");
             }
-            else if(LHFingers == " 1 1 0 0 0")
+            else if (RHFingers == "1 1 0 0 0")
             {
-                Vector3 temp = new Vector3(0.01f, 0, 0); //scaling x axis
+                Vector3 temp = new Vector3(0.01f, 0.01f, 0.01f); //scaling x axis
                 handPoints.transform.localScale += temp;
                 print("Scale");
             }
@@ -79,76 +57,83 @@ public class Positioning : MonoBehaviour
                 print("Not Recognized");
             }
         }
-  
-        if (points.Length == 16)
+
+        if (points.Length == 13)
         {
-            handType2 = points[14].ToString();
-            print(handType2);
+            handType = points[0];
+            if (handType == "Left")
+            {
+                LHFingers = points[1] + points[2] + points[3] + points[4] + points[5];
+                RHFingers2 = points[7] + points[8] + points[9] + points[10] + points[11];
+            }
+            else
+            {
+                LHFingers = points[7] + points[8] + points[9] + points[10] + points[11];
+                RHFingers2 = points[1] + points[2] + points[3] + points[4] + points[5];
+            }
 
-            RHFingers = points[4] + points[5] + points[6] + points[7] + points[8];
-            LHFingers2 = points[9] + points[10] + points[11] + points[12] + points[13];
-            print(RHFingers);
 
-            positionXR = int.Parse(points[0]); print(positionXR);
-            positionYR = int.Parse(points[1]); print(positionYR);
+            print(RHFingers2);
+            print(LHFingers);
 
-            if (RHFingers == " 1 0 1 1 1" && LHFingers2 == " 0 0 0 0 0")
+
+            if (RHFingers2 == " 1 1 1 1 1" && LHFingers == " 1 0 0 0 1")
             {
                 Vector3 temp = new Vector3(0, 0, 0.01f); //grab z axis
                 handPoints.transform.position += temp;
                 print("Positioning Z axis");
             }
 
-            else if (RHFingers == " 0 1 1 0 0" && LHFingers2 == " 0 0 0 0 0")
+            else if (RHFingers2 == " 1 1 1 1 1" && LHFingers == " 0 1 1 0 0")
             {
                 Vector3 temp = new Vector3(0, 0.01f, 0); //grab y axis
                 handPoints.transform.position += temp;
                 print("Positioning Y axis");
             }
 
-            else if (RHFingers == " 1 0 0 0 1" && LHFingers2 == " 0 0 0 0 0")
+            else if (RHFingers2 == " 1 1 1 1 1" && LHFingers == " 1 0 0 0 0")
             {
                 Vector3 temp = new Vector3(0.01f, 0, 0); //grab x axis
                 handPoints.transform.position += temp;
                 print("Positioning X axis");
             }
 
-            else if (RHFingers == " 1 0 1 1 1" && LHFingers2 == " 1 0 0 0 0")
+            else if (RHFingers2 == " 0 0 0 0 0" && LHFingers == " 1 0 0 0 1")
             {
                 Vector3 temp = new Vector3(0, 0, 0.1f); //rotate z axis
                 handPoints.transform.Rotate(temp);
                 print("Rotation Z axis");
             }
 
-            else if (RHFingers == " 0 1 1 0 0" && LHFingers2 == " 1 0 0 0 0")
+            else if (RHFingers2 == " 0 0 0 0 0" && LHFingers == " 0 1 1 0 0")
             {
                 Vector3 temp = new Vector3(0, 0.1f, 0); //rotate y axis
                 handPoints.transform.Rotate(temp);
                 print("Rotation Y axis");
             }
 
-            else if (RHFingers == " 1 0 0 0 1" && LHFingers2 == " 1 0 0 0 0")
+            else if (RHFingers2 == " 0 0 0 0 0" && LHFingers == " 1 0 0 0 0")
             {
                 Vector3 temp = new Vector3(0.1f, 0, 0); //rotate x axis
                 handPoints.transform.Rotate(temp);
                 print("Rotation X axis");
             }
 
-            else if (RHFingers == " 1 0 1 1 1" && LHFingers2 == " 1 1 0 0 0")
+            else if (RHFingers2 == " 1 1 0 0 0" && LHFingers == " 1 0 0 0 1")
             {
                 Vector3 temp = new Vector3(0, 0, 0.01f); //scaling z axis
                 handPoints.transform.localScale += temp;
                 print("Scale Z axis");
             }
 
-            else if (RHFingers == " 0 1 1 0 0" && LHFingers2 == " 1 1 0 0 0")
+            else if (RHFingers2 == " 1 1 0 0 0" && LHFingers == " 0 1 1 0 0")
             {
                 Vector3 temp = new Vector3(0, 0.01f, 0); //scaling y axis
                 handPoints.transform.localScale += temp;
                 print("Scale Y axis");
             }
 
-            else if (RHFingers == " 1 0 0 0 1" && LHFingers2 == " 1 1 0 0 0")
+            else if (RHFingers2 == " 1 1 0 0 0" && LHFingers == " 1 0 0 0 0")
             {
                 Vector3 temp = new Vector3(0.01f, 0, 0); //scaling x axis
                 handPoints.transform.localScale += temp;
@@ -158,7 +143,7 @@ public class Positioning : MonoBehaviour
             {
                 print("Not Recognized");
             }
-            
+
         }
     }
 }
